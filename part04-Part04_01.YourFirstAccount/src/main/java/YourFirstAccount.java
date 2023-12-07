cb356cd9baa6d75d0bdf1b3feb809d4105fab53c
memendoza60 <151693491+memendoza60@users.noreply.github.com>
2023-12-07 15:32:41 -0800

    Add files via upload

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account mylesAccount = new Account("Myles's account", 100.00);
+        mylesAccount.deposit(20);
+        System.out.println(mylesAccount);
+    }
+}
